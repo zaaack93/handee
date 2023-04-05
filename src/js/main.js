975cd7e380ca0629034333593cf60c7ead6c71a3
ZBOUZANGAD <bouzanagdzakaria@gmail.com>
2023-04-05 10:24:30 +0000

    Header Blur

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+//header navigation trigger
+const targetElement = document.querySelector(".blur-bg-header");
+console.log(targetElement)
+if(targetElement){
+window.addEventListener("scroll", () => {
+  if (window.scrollY === 0) {
+    targetElement.classList.add("no-back-blur");
+  } else {
+    targetElement.classList.remove("no-back-blur");
+  }
+});
+window.addEventListener("load", function () {
+  if (window.scrollY === 0) {
+    targetElement.classList.add("no-back-blur");
+  } else {
+    targetElement.classList.remove("no-back-blur");
+  }
+});
+}
